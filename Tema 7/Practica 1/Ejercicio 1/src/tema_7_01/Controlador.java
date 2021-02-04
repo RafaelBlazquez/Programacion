6daa055f9a30ab8f789ea038f2720bea1c4b8ece
RafaelBlazquez <73467706+RafaelBlazquez@users.noreply.github.com>
2021-02-04 16:53:48 +0100

    Add files via upload

---
+++ Tema 7/Practica 1/Ejercicio 1/src/tema_7_01/Controlador.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tema_7_01;
+
+import Vista.*;
+
+public class Controlador {
+
+    private static V1 v1;
+  
+    public static void main(String[] args) {
+        v1 = new V1();
+        v1.setVisible(true);  
+    }
+    public static void salir(){
+        v1.dispose();
+        System.exit(0);
+    }
+    
+}
